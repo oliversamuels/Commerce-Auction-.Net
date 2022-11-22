4c22c218573f4012fd507248703a612f5dd208b9
Oliversamuels <amoskevinsamuel@yahoo.com>
2022-11-22 11:03:13 +0100

    Correcting

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
 builder.Services.AddControllersWithViews();
 
-app.MapGet("/", () => "Hello World!");
+
+var app = builder.Build();
+//app.MapGet("/", () => "Hello World!");
 
 app.UseStaticFiles();
 app.MapDefaultControllerRoute();
