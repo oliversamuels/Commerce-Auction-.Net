3e19abf505b10223b6b494aa92322d474d69498f
Oliversamuels <amoskevinsamuel@yahoo.com>
2022-11-24 13:21:50 +0100

    Day 2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,10 +6,32 @@ namespace Commerce.Controllers;
 public class HomeController : Controller
 {
     private IListingRepository repository;
+    private DataContext _db;
 
-    public HomeController(IListingRepository repo)
+    public HomeController(IListingRepository repo, DataContext db)
     {
         repository = repo;
+        _db = db;
     }
     public IActionResult Index() => View(repository.Listings);
+    
+    public IActionResult Create() => View();
+
+    [HttpPost]
+    public RedirectResult Create(Listing auctionlist)
+    {
+        if(ModelState.IsValid)
+        {
+            _db.Add(auctionlist);
+            _db.SaveChanges();
+        }
+        return Redirect("/");
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult Listing(long id)
+    {
+        var contx = repository.Listings.Where(d => d.ListingId.Equals(id));
+        return View(contx);
+    }
 }
