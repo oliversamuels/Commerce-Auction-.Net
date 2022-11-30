c5c11734c03cb517019b0c8c7795c5c0e33e7129
Oliversamuels <amoskevinsamuel@yahoo.com>
2022-11-30 14:28:37 +0100

    Com

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -7,6 +7,8 @@ public class Comment
     public long CommentId {get; set; }
     public string Name {get; set; } = string.Empty;
     public string Review {get; set; } = string.Empty;
+    [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{dd-MM-yyyy hh:mm tt}")]
+    public DateTime DateAdded { get; set; } = DateTime.Now;
     public long ListingId {get; set; }
     public virtual Listing? Listing { get; set; }
 }
