fdf281cf8a5ea134d522f4c38f9224b15d32d9d8
Oliversamuels <amoskevinsamuel@yahoo.com>
2022-12-05 08:34:14 +0100

    Update - User Identity

---
+++ Migrations/Identity/20221205072929_PropertyII.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Commerce.Migrations.Identity
+{
+    public partial class PropertyII : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Models/Users.cs
+++ Models/Users.cs
@@ -7,16 +7,13 @@ namespace Commerce.Models;
 public class Users
 {
     [BindProperty]
-    [Required]
     public string UserName {get; set; } = string.Empty;
 
     [BindProperty]
-    [Required]
     [DataType(DataType.EmailAddress)]
     public string Email {get; set; } = string.Empty;
 
     [BindProperty]
-    [Required]
     [DataType(DataType.Password)]
     public string Password {get; set; } = string.Empty;
 }
