58a739aa7ef54c5e275bd212275d0e44fe83041e
Oliversamuels <amoskevinsamuel@yahoo.com>
2022-12-05 08:23:28 +0100

    DataAnnotations Complete

---
+++ Migrations/Identity/20221205071054_Property.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Commerce.Migrations.Identity
+{
+    public partial class Property : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Models/Users.cs
+++ Models/Users.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace Commerce.Models;
@@ -6,11 +7,16 @@ namespace Commerce.Models;
 public class Users
 {
     [BindProperty]
+    [Required]
     public string UserName {get; set; } = string.Empty;
 
     [BindProperty]
+    [Required]
+    [DataType(DataType.EmailAddress)]
     public string Email {get; set; } = string.Empty;
 
     [BindProperty]
+    [Required]
+    [DataType(DataType.Password)]
     public string Password {get; set; } = string.Empty;
 }
